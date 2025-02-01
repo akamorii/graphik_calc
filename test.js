53fc882a7467b794b9731dad07bedb35cd5d6599
akamorii <cat015b@mail.ru>
2025-02-01 22:25:30 +0300

    try to make graph

--- test.js
+++ test.js
@@ -1,5 +1,27 @@
 'use strict'
 
-let zxc = {'q':1, 'qq':11, 'qqq':111}
-delete zxc['qq']
-console.log(Object.keys(zxc));
+class Graph {
+    constructor() {
+        this.nodes = [[0]];
+    }
+    graphSize() {
+        return this.nodes.length
+    }
+
+    createNode(vertexSet) {
+        const vertexCount = vertexSet.length;
+        console.log(vertexSet);
+        this.nodes.push([])
+        if (vertexSet) {
+            for(let i=0; i<vertexSet.length; i++){
+                
+            }
+        }
+        // this.nodes[].push()
+    }
+    // nodes: [[0]],
+    // function createNode()
+}
+
+const grp = new Graph()
+grp.createNode((1,2,3))
